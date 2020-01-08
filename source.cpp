4060dba90a95b275671416dbd9df397600c8fabf
dejvi-S <xzajcew@interia.pl>
2020-01-08 17:40:29 +0100

    Working search element, problem with result solving

--- source.cpp
+++ source.cpp
@@ -21,17 +21,27 @@ public:
     bool diagonalCheckZero(unsigned int index);
     double matrixFactor(unsigned int index, unsigned int offsetIndex);
     void rowCalculation(unsigned int index);
-    bool gaussMainCalculations(unsigned int &index);
+    bool gaussMainCalculations(unsigned int index);
     void basicGauss();
     void printResult(bool successSwitch);
     bool maxElementInRow(unsigned int index);
+    bool maxElementInColumn(unsigned int index);
+    void maxElementInMatrix(unsigned int index);
+    bool gaussInColumn();
+    bool gaussInRow();
+    bool gaussFull();
 };
 
 int main()
 {
     Gauss g1 = Gauss("test.csv");
+    Gauss g2 = Gauss(g1),g3 = Gauss(g1),g4 = Gauss(g1);
     g1.printMatrix();
     g1.basicGauss();
+    g2.gaussInRow();
+    g3.gaussInColumn();
+    g4.gaussFull();
+
 
     return 0;
 }
@@ -113,30 +123,34 @@ void Gauss::rowCalculation(unsigned int index) {
                 matrixVectors[i+1][j] = 0;
     }
 }
-bool Gauss::gaussMainCalculations(unsigned int &index)
+bool Gauss::gaussMainCalculations(unsigned int index)
 {
-    for(int i=index; i<matrixSize-1; i++)
-    {
-        if(!diagonalCheckZero(i)) {
-            rowCalculation(i);
-            for(int i=matrixSize-1; i>=0; i--)
-            {
-                double sum = 0;
-                for(int j=matrixSize-1; j>i; j--)
-                    sum = sum+result[j] * matrixVectors[i][j];
-                result[i] = (-sum + matrixVectors[i][matrixSize]) / matrixVectors[i][i];
-            }
-        }
-        else {
-            index = i;
-            return false;
-        }
+    if(!diagonalCheckZero(index)) {
+        rowCalculation(index);
+       //kalkulation
+        return true;
     }
-    return true;
+    else
+        return false;
 }
 void Gauss::basicGauss() {
-    unsigned int lastIndex = 0;
-    (gaussMainCalculations(lastIndex)) ? printResult(true) : printResult(false);
+    bool breaked = false;
+    for(int i=0; i<matrixSize-1; i++) {
+        if(!gaussMainCalculations(i))
+            break;
+    }
+    if(breaked)
+        printResult(false);
+    else {
+        for(int i=matrixSize-1; i>=0; i--)
+        {
+            double sum = 0;
+            for(int j=matrixSize-1; j>i; j--)
+                sum = sum+result[j] * matrixVectors[i][j];
+            result[i] = (-sum + matrixVectors[i][matrixSize]) / matrixVectors[i][i];
+        }
+        printResult(true);
+    }
 }
 void Gauss::printResult(bool successSwitch){
     if(successSwitch) {
@@ -155,23 +169,93 @@ void Gauss::printResult(bool successSwitch){
     } else cout<<"Macierz nieosobliwa!\n";
 }
 bool Gauss::maxElementInRow(unsigned int index)
-    {
-        double tempMaxElement = fabs(matrixVectors[index][index]);
-        unsigned int swapIndex = index;
-        for(int i=index+1;i<matrixSize;i++)
-            if(tempMaxElement<fabs(matrixVectors[i][index]))
-            {
-                tempMaxElement = fabs(matrixVectors[i][index]);
-                swapIndex = i;
-            }
-        if(index == swapIndex) {
-            return false;
+{
+    double tempMaxElement = fabs(matrixVectors[index][index]);
+    unsigned int swapIndex = index;
+    for(int i=index+1;i<matrixSize;i++)
+        if(tempMaxElement < fabs(matrixVectors[index][i]))    {
+            tempMaxElement = fabs(matrixVectors[index][i]);
+            swapIndex = i;
         }
-        else {
-            swapStack.push_back(index);
-            swapStack.push_back(swapIndex);
-            for(int i=0; i<matrixSize; i++)
-                swap(matrixVectors[i][swapIndex],matrixVectors[i][index]);
-            return true;
-        }
+    if(index == swapIndex) {
+        return false;
     }
+    else {
+        swapStack.push_back(index);
+        swapStack.push_back(swapIndex);
+        for(int i=index; i<matrixSize; i++)
+            swap(matrixVectors[i][swapIndex],matrixVectors[i][index]);
+        return true;
+    }
+}
+bool Gauss::maxElementInColumn(unsigned int index)
+{
+    double tempMaxElement = fabs(matrixVectors[index][index]);
+    unsigned int swapIndex = index;
+    for(int i=index+1;i<matrixSize;i++)
+        if(tempMaxElement<fabs(matrixVectors[i][index]))
+        {
+            tempMaxElement = fabs(matrixVectors[i][index]);
+            swapIndex = i;
+        }
+    if(index == swapIndex)
+        return false;
+    else {
+        for(int i=index; i<=matrixSize; i++)
+            swap(matrixVectors[swapIndex][i],matrixVectors[index][i]);
+        return true;
+    }
+}
+void Gauss::maxElementInMatrix(unsigned int index)
+{
+    double tempMaxElement = fabs(matrixVectors[index][index]);
+    unsigned int swapIndexJ = index;
+    unsigned int swapIndexI = index;
+    for(int i=index;i<matrixSize;i++)
+        for(int j=index;j<matrixSize;j++)
+            if(tempMaxElement<fabs(matrixVectors[i][j]))
+            {
+                tempMaxElement = fabs(matrixVectors[i][j]);
+                swapIndexJ = j;
+                swapIndexI = i;
+            }
+    if(index != swapIndexI) {
+        for(int i=index; i<=matrixSize; i++)
+            swap(matrixVectors[swapIndexI][i],matrixVectors[index][i]);
+    }
+    if(index != swapIndexJ) {
+        swapStack.push_back(index);
+        swapStack.push_back(swapIndexJ);
+        for(int i=index; i<matrixSize; i++)
+            swap(matrixVectors[i][swapIndexJ],matrixVectors[i][index]);
+    }
+}
+bool Gauss::gaussInColumn()
+{
+    for(int i=0; i<matrixSize-1; i++) {
+        maxElementInColumn(i);
+        gaussMainCalculations(i);
+    }
+}
+bool Gauss::gaussInRow()
+{
+    for(int i=0; i<matrixSize-1; i++) {
+        maxElementInRow(i);
+        gaussMainCalculations(i);
+    }
+    for(int i=matrixSize-1; i>=0; i--)
+    {
+        double sum = 0;
+        for(int j=matrixSize-1; j>i; j--)
+            sum = sum+result[j] * matrixVectors[i][j];
+        result[i] = (-sum + matrixVectors[i][matrixSize]) / matrixVectors[i][i];
+    }
+    printResult(true);
+}
+bool Gauss::gaussFull()
+{
+    for(int i=0; i<matrixSize-1; i++) {
+        maxElementInMatrix(i);
+        gaussMainCalculations(i);
+    }
+}
